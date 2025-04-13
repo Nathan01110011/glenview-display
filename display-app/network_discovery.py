8056c2419d1c7d0af58ac11e649a3d887f9ba869
Nathan <nathanscott101@gmail.com>
2025-04-13 10:10:06 +0100

    adjusting ip range

--- display-app/network_discovery.py
+++ display-app/network_discovery.py
@@ -7,7 +7,7 @@ def find_server_on_lan(port=8000, timeout=0.2):
     preferred_subnets = ["192.168.1", "192.168.0"]
     for subnet in preferred_subnets:
         print(f"🔍 Scanning {subnet}.1–{subnet}.20...")
-        for i in range(1, 21):
+        for i in range(100, 130):
             ip = f"{subnet}.{i}"
             try:
                 r = httpx.get(f"http://{ip}:{port}/state", timeout=timeout)
